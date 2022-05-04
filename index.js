ef87adfeaefaabbad4d3c4a3075afe7161281c1a
GAMBILLJJ94 <100322957+GAMBILLJJ94@users.noreply.github.com>
2022-05-04 15:57:17 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+//var officeItems = ["stapler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer",  "computer"]
+
+//for (i = 0; i < officeItems.length; i++){
+ //   console.log(officeItems[i])
+//}
+
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+    {
+      name: "Mike",
+      age: 12,
+      gender: "male"
+    },{
+      name: "Madeline",
+      age: 80,
+      gender: "female"
+    },{
+      name: "Cheryl",
+      age: 22,
+      gender: "female"
+    },{
+      name: "Sam",
+      age: 30,
+      gender: "male"
+    },{
+      name: "Suzy",
+      age: 4,
+      gender: "female"
+    }
+  ]
+  
+  //for (var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
+   //   if (peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18){
+   //       console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is old enough to see Mad Max.")
+    //  } else if (peopleWhoWantToSeeMadMaxFuryRoad[i].age < 18){
+    //      console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name + " is not old enough to see Mad //Max.")
+    //  }
+  //}
+  
+  var numbersArray = [2,3,2]
+  
+  ///function addAll (array){
+  //    console.log; parseInt(array.length)
+  //}
+  
+  //addAll(numbersArray)
+  
+  for (var i = 0; i < numbersArray.length; i++){
+      if (numbersArray[i] % 2 === 0 ){
+          console.log("the light is off.")
+      } else {
+          console.log("the light is on. ")
+      }
+  }
